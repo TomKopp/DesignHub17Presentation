ccffe9d936e194654a07850e1f09c69b85b24d73
Tom Kopp <witzbould@live.com>
2018-03-21 12:01:08 +0100

    import perlin noise and modify render

--- src/views/index.js
+++ src/views/index.js
@@ -4,15 +4,13 @@ const { readFile, writeFileSync } = require('fs');
 const h = require('hyperscript');
 const csv = require('fast-csv');
 const { Rekapi, Actor } = require('rekapi');
+const { Noise } = require('noisejs');
 const { loggerStdout } = require(join(process.cwd(), 'src', 'utils', 'utils.js'));
-const PositionMarker = require(join(process.cwd(), 'src', 'utils', 'PositionMarker.js'));
 const signals = require(join(process.cwd(), 'config', 'signals.js'));
 
 
 let firstTimeStamp = null;
 const dancers = new Map();
-// const distances = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
-const distances = [40, 40, 40, 40, 40, 40, 40, 40, 40, 40];
 const filePath = join(process.cwd(), 'assets', 'danceData.csv');
 const filePathProcessed = join(process.cwd(), 'assets', 'danceData.processed.json');
 const [ winContentSizeWidth, winContentSizeHeight ] = remote.getCurrentWindow().getContentSize();
@@ -29,6 +27,7 @@ const canvas = h(
 );
 
 const rekapi = new Rekapi(canvas.getContext('2d'));
+const myNoise = new Noise(Math.random());
 
 const playPause = () => {
 	if (rekapi.isPlaying()) {
@@ -64,15 +63,6 @@ const addPathsToDancers = ([
 	dancers.get(dancerId).push([ x, y, timestamp ]);
 };
 
-const addPlayPauseHandler = () => {
-	ipcRenderer.on('signal', (event, message) => {
-		if (signals.get('playPause') === message) {
-			playPause();
-		}
-		loggerStdout(message);
-	});
-};
-
 const normalizeDancePaths = (dancePathsMap) => {
 	// projectionWidth in meter @TODO - move to settings
 	const projectionWidth = 3;
@@ -89,7 +79,7 @@ const normalizeDancePaths = (dancePathsMap) => {
 	});
 	avg /= numberOfElements;
 	dancePathsMap.forEach((val) => {
-		val.forEach(([x, z, timestamp], i, arr) => {
+		val.forEach(([ x, z, timestamp ], i, arr) => {
 			let time = 0;
 
 			if (firstTimeStamp === null) {
@@ -98,17 +88,21 @@ const normalizeDancePaths = (dancePathsMap) => {
 				time = timestamp - firstTimeStamp;
 			}
 
-			arr[i] = [x * widthCoefficient + winContentSizeWidth / 2, (z - avg) * heightCoefficient + winContentSizeHeight / 2, time];
+			arr[i] = [ x * widthCoefficient + winContentSizeWidth / 2, (z - avg) * heightCoefficient + winContentSizeHeight / 2, time ];
 		});
 	});
 };
 
-const getRandomIntInclusive = (min, max) => {
-	min = Math.ceil(min);
-	max = Math.floor(max);
+// const getRandomIntInclusive = (min, max) => {
+// 	min = Math.ceil(min);
+// 	max = Math.floor(max);
 
-	// The maximum is inclusive and the minimum is inclusive
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+// 	// The maximum is inclusive and the minimum is inclusive
+// 	return Math.floor(Math.random() * (max - min + 1)) + min;
+// };
+
+const myMapping = (floatVal, min, max, a, b) => {
+	return (b - a) * (floatVal - min) / (max - min) + a;
 };
 
 const importTimeline = (data) => {
@@ -116,6 +110,17 @@ const importTimeline = (data) => {
 
 	rekapi.importTimeline(timeline);
 };
+
+// 2nd dimension of perlin noise
+let yoff = 0.0;
+const r = 60;
+const angle = 360;
+const steps = 60;
+// 0 - 1
+const amplitude = 0.5;
+// 0.1 - 30
+const intensity = 5;
+const speed = 0.02;
 
 /* eslint max-statements: "off" */
 const renderMyShit = (ctx, state) => {
@@ -130,26 +135,35 @@ const renderMyShit = (ctx, state) => {
 	ctx.arc(state.x, state.y, state.radius, 0, Math.PI * 2);
 	ctx.closePath();
 
-	ctx.setLineDash([2, 5]);
+	ctx.setLineDash([ 2, 5 ]);
 	ctx.strokeStyle = 'rgba(255, 255, 255, 0.7)';
 	ctx.stroke();
 
 	ctx.beginPath();
-	ctx.moveTo(state.p0x, state.p0y);
-	ctx.lineTo(state.p1x, state.p1y);
-	ctx.lineTo(state.p2x, state.p2y);
-	ctx.lineTo(state.p3x, state.p3y);
-	ctx.lineTo(state.p4x, state.p4y);
-	ctx.lineTo(state.p5x, state.p5y);
-	ctx.lineTo(state.p6x, state.p6y);
-	ctx.lineTo(state.p7x, state.p7y);
-	ctx.lineTo(state.p8x, state.p8y);
-	ctx.lineTo(state.p9x, state.p9y);
+
+	for (let i = 0; i < angle; i += angle / steps) {
+		const rad = 2 * Math.PI * i / angle;
+		const distance = r * myMapping(myNoise.perlin3(Math.cos(rad) * intensity + 1, Math.sin(rad) * intensity + 1, yoff), -1, 1, amplitude, 1);
+		const x = state.x + Math.cos(rad) * distance;
+		const y = state.y + Math.sin(rad) * distance;
+
+		ctx.lineTo(x, y);
+	}
+	yoff += speed;
+
 	ctx.closePath();
 
 	ctx.setLineDash([]);
-	ctx.strokeStyle = state.color;
+	ctx.strokeStyle = 'rgba(255, 92, 57, 1)';
+	const gradient = ctx.createRadialGradient(state.x, state.y, 0, state.x, state.y, 60);
+
+	gradient.addColorStop(0, 'rgba(255, 92, 57, 0.0)');
+	gradient.addColorStop(0.5, 'rgba(255, 92, 57, 0.2)');
+	gradient.addColorStop(0.7, 'rgba(255, 92, 57, 0.4)');
+	gradient.addColorStop(1, 'rgba(255, 92, 57, 1)');
+	ctx.fillStyle = gradient;
 	ctx.stroke();
+	ctx.fill();
 
 
 	ctx.restore();
@@ -161,21 +175,7 @@ const doMyShit = () => {
 
 		dancerPath.forEach(([ x, y, timestamp ]) => {
 			if (actor.hasKeyframeAt(timestamp) === false) {
-
-				const myArr = new Array(10);
-
-				distances.forEach((el, key) => {
-					myArr[key] = el + getRandomIntInclusive(-5, 5);
-				});
-
-				// const bla = new PositionMarker(x, y, distances);
-				const bla = new PositionMarker(x, y, myArr);
-
-				actor.keyframe(timestamp, bla);
-
-				// bla.dancerId = dancerId;
-				// bla.time = timestamp;
-				// csvStream.write(bla);
+				actor.keyframe(timestamp, { x, y, shouldRender: true, radius: 50 });
 			}
 		});
 
@@ -206,6 +206,12 @@ const loadProcessCSV = () => {
 			normalizeDancePaths(dancers);
 
 			doMyShit();
+
+			const baum = document.getElementById('loading');
+
+			if (baum) {
+				baum.remove();
+			}
 		});
 };
 
@@ -232,7 +238,9 @@ readFile(filePathProcessed, (err, data) => {
 
 	try {
 		importTimeline(data);
-		rekapi.getAllActors().forEach((el) => el.render = renderMyShit);
+		rekapi.getAllActors().forEach((el) => {
+			el.render = renderMyShit;
+		});
 	} catch (error) {
 		loggerStdout(error);
 		// Load csv file and process it
@@ -240,8 +248,13 @@ readFile(filePathProcessed, (err, data) => {
 	}
 
 	rekapi.on('animationComplete', () => loggerStdout('animation complete'));
-	addPlayPauseHandler();
 	document.getElementById('loading').remove();
 });
 
+ipcRenderer.on('signal', (event, message) => {
+	if (signals.get('playPause') === message) {
+		playPause();
+	}
+	loggerStdout(message);
+});
 
